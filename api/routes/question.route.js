ff0fdda4a31f971a0b1cb82cfd1d2ed8f8a4f2a5
jkaus324 <92943010+jkaus324@users.noreply.github.com>
2023-11-13 23:58:19 +0530

    Did some changes in backend

--- api/routes/question.route.js
+++ api/routes/question.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { addQuestion, getQuestion } from '../controllers/question.controller';
 
 const router = express.Router();
 
-router.post('/add');
-router.post('/get');
+router.post('/add',addQuestion);
+router.post('/get',getQuestion);
 
 export default router;
