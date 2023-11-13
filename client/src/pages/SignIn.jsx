135202bdf63c42b54da2ff74833e18b402654768
jkaus324 <92943010+jkaus324@users.noreply.github.com>
2023-11-13 22:43:05 +0530

    Created Backend

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignIn() {
   return (
-    <div>
-      <h3>SignIn</h3>
+    <div className='container p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-weight-bold my-7'>Sign In</h1>
+      <form className='mb-4'>
+        <div className='mb-3'>
+          <input
+            type='text'
+            placeholder='Email'
+            className='form-control'
+            id='email'
+          />
+        </div>
+        <div className='mb-3'>
+          <input
+            type='password'
+            placeholder='Password'
+            className='form-control'
+            id='password'
+          />
+        </div>
+        <button
+          type='submit'
+          className='btn btn-primary btn-block'
+        >
+          Sign In
+        </button>
+      </form>
+      <div className='d-flex gap-2 mt-5'>
+        <p>Don't have an account?</p>
+        <Link to='/sign-up' className='text-primary'>
+          Sign Up
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
   return (
-    <div>
-      <h1>SignUp</h1>
+    <div className='container p-3'>
+      <h1 className='text-center font-weight-bold my-4'>Signup</h1>
+      <form  className='mb-4'>
+        <div className='mb-3'>
+          <input
+            type='text'
+            placeholder='Username'
+            className='form-control'
+            id='username'
+          />
+        </div>
+        <div className='mb-3'>
+          <input
+            type='text'
+            placeholder='Email'
+            className='form-control'
+            id='email'
+          />
+        </div>
+        <div className='mb-3'>
+          <input
+            type='text'
+            placeholder='Password'
+            className='form-control'
+            id='password'
+          />
+        </div>
+        <button
+          type='submit'
+          className='btn btn-primary btn-block'>
+          Sign Up
+        </button>
+      </form>
+      <div className='d-flex justify-content-center'>
+        <p className='mr-2'>Have an account?</p>
+        <Link to='/sign-in' className='text-primary'>
+          Sign In
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
